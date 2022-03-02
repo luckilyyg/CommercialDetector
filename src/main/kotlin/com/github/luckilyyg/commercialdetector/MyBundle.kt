8947632bb02d693bb4b01fc018c30d042a2dba8d
GitHub Action <action@github.com>
2022-03-02 02:10:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/luckilyyg/commercialdetector/MyBundle.kt
+++ src/main/kotlin/com/github/luckilyyg/commercialdetector/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.luckilyyg.commercialdetector
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/luckilyyg/commercialdetector/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.luckilyyg.commercialdetector.services
+
+import com.github.luckilyyg.commercialdetector.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
